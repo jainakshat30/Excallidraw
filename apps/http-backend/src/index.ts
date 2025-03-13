fc4d42e5134e2b5056ea7aabad02715a4d53b03f
Akshat Jain <jain.akshat.0730@gmail.com>
2025-03-13 22:38:04 +0530

    added the common zod package and the backend-common package

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,17 +1,31 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
+
 
 const app = express();
 
 app.post("/signup",(req,res)=>{
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.status(400).json({message:"Invalid request"});
+        }
+return;
     res.json({
         userd: 123
     })
 })
 
 app.post("/signin",(req,res)=>{
+
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.status(400).json({message:"Invalid request"});
+        }
+        return;
+
     const userId = 1;
     const token = jwt.sign({
         userId
@@ -23,6 +37,13 @@ app.post("/signin",(req,res)=>{
 })
 
 app.post("/room",middleware,(req,res)=>{
+
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.status(400).json({message:"Invalid request"});
+        }
+        return;
+
     res.json({
         roomId : 123
     })
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { decode, JwtPayload } from "jsonwebtoken"
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 interface CustomRequest extends Request {
     userId?: string;
