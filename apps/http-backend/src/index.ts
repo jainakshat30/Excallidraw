3c7f608c7350eb666e1fb091f24a498a4e821e33
Akshat Jain <jain.akshat.0730@gmail.com>
2025-03-16 00:18:59 +0530

    added the signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,19 +3,36 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
-
+import { prismaClient } from "@repo/db/client"
 
 const app = express();
 
 app.post("/signup",(req,res)=>{
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success){
         res.status(400).json({message:"Invalid request"});
-        }
+        
 return;
+}
+try {
+    await prismaClient.user.create({
+        data: {
+            email: parsedData.data?.username,
+            password: parsedData.data?.password,
+            name : parsedData.data?.name
+        }
+        
+
+    })
     res.json({
         userd: 123
     })
+}catch(e){
+    res.status(411).json({
+        message: "Error creating user"
+    })
+}
+    
 })
 
 app.post("/signin",(req,res)=>{
@@ -23,9 +40,9 @@ app.post("/signin",(req,res)=>{
     const data = SigninSchema.safeParse(req.body);
     if(!data.success){
         res.status(400).json({message:"Invalid request"});
-        }
+        
         return;
-
+    }
     const userId = 1;
     const token = jwt.sign({
         userId
@@ -41,9 +58,9 @@ app.post("/room",middleware,(req,res)=>{
     const data = CreateRoomSchema.safeParse(req.body);
     if(!data.success){
         res.status(400).json({message:"Invalid request"});
-        }
+       
         return;
-
+    }
     res.json({
         roomId : 123
     })
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -12,6 +12,11 @@ wss.on('connection', function connection(ws,request) {
     const queryParams = new URLSearchParams(url.split("?")[1]);
     const token = queryParams.get('token') || "";
     const decoded = jwt.verify(token,JWT_SECRET)
+    
+    if(typeof decoded == "string"){
+        ws.close();
+        return;
+    }
 
     if(!decoded || !(decoded as JwtPayload).userId){
         ws.close();
